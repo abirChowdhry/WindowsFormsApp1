323ca948895376ba1270b9305f5c171fa89844a1
abirChowdhry <77957558+abirChowdhry@users.noreply.github.com>
2021-11-09 13:10:34 +0600

    Add project files.

---
+++ WindowsFormsApp1/MyForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            /* if (button1.Text == "Click")
+                 button1.Text = "Hello";
+             else
+                 button1.Text = "Click";
+            */
+
+            string result = Convert.ToString(Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text));
+            MessageBox.Show("Summation: "+result);
+        }
+
+        private void MyForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
